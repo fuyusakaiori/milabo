7608d2cada970c1c0a297ebe01d4a7991f4d6ed1
fuyusakaiori <670232228@qq.com>
2023-07-14 01:10:15 +0800

    reverse proxy

---
+++ neptune-gateway/demo/proxy/real_server/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+const (
+	StatusServerError int = 500
+)
+
+type RealServer struct {
+	// 服务端地址
+	RemoteAddr string
+}
+
+func (server *RealServer) Run() {
+	// 1. 输出启动日志
+	log.Printf("starting http server :%s", server.RemoteAddr)
+	// 2. 创建路由
+	router := http.NewServeMux()
+	// 3. 绑定函数
+	router.HandleFunc("/", server.HelloHandler)
+	router.HandleFunc("/base/error", server.ErrorHandler)
+	router.HandleFunc("/base/timeout", server.TimeoutHandler)
+	// 4. 配置服务器
+	realServer := &http.Server{
+		Addr: server.RemoteAddr,
+		// 写入超时: 从接收 HTTP Request Header结束到完成 HTTP Response 写入结束为止所花的时间
+		WriteTimeout: time.Second * 10,
+		Handler:      router,
+	}
+	// 5. 协程启动
+	go func() {
+		log.Fatalln(realServer.ListenAndServe())
+	}()
+}
+
+func (server *RealServer) HelloHandler(responseWriter http.ResponseWriter, request *http.Request) {
+	urlAndPath := fmt.Sprintf("https://%s%s\n", request.RemoteAddr, request.URL.Path)
+	if _, err := io.WriteString(responseWriter, urlAndPath); err != nil {
+		log.Fatalf("url and path write response writer err: %v", err)
+	}
+
+}
+
+func (server *RealServer) ErrorHandler(responseWriter http.ResponseWriter, request *http.Request) {
+	responseWriter.WriteHeader(StatusServerError)
+	if _, err := io.WriteString(responseWriter, "error handler"); err != nil {
+		log.Fatalf("url and path write response writer err: %v", err)
+	}
+}
+
+func (server *RealServer) TimeoutHandler(responseWriter http.ResponseWriter, request *http.Request) {
+	time.Sleep(time.Second * 6)
+	responseWriter.WriteHeader(http.StatusOK)
+	if _, err := io.WriteString(responseWriter, "timeout handler"); err != nil {
+		log.Fatalf("url and path write response writer err: %v", err)
+	}
+}
+
+func main() {
+	// 1. 创建服务器
+	realServerA := &RealServer{RemoteAddr: "127.0.0.1:8081"}
+	realServerA.Run()
+	realServerB := &RealServer{RemoteAddr: "127.0.0.1:8082"}
+	realServerB.Run()
+	// 2. 创建信号管道
+	quit := make(chan os.Signal)
+	// 3. 监听信号
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	// 4. 获取监听到的信号
+	<-quit
+}
---
+++ neptune-gateway/demo/proxy/reverse_prxoy_base/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"net/http"
+	"net/url"
+)
+
+const (
+	proxyServerAddr   string = "127.0.0.1:8080"
+	beProxyServerAddr        = "http://127.0.0.1:8081"
+)
+
+type ProxyServer struct {
+	RemoteAddr string
+}
+
+func (server *ProxyServer) Run() {
+	log.Printf("starting proxy server :%s", server.RemoteAddr)
+	router := http.NewServeMux()
+	router.HandleFunc("/", server.handler)
+	realServer := &http.Server{
+		Addr:    server.RemoteAddr,
+		Handler: router,
+	}
+	log.Fatalln(realServer.ListenAndServe())
+}
+
+func (server *ProxyServer) handler(responseWriter http.ResponseWriter, request *http.Request) {
+	// 1. 解析被代理的服务器的地址
+	beProxy, err := url.Parse(beProxyServerAddr)
+	if err != nil {
+		return
+	}
+	// 2. 修改请求体内容 - 之前客户端请求的是代理服务器而不是真实服务器
+	request.URL.Scheme = beProxy.Scheme
+	request.URL.Host = beProxy.Host
+	// 3. 向被代理的服务器发送请求
+	tcp := http.DefaultTransport
+	response, err := tcp.RoundTrip(request)
+	defer response.Body.Close()
+	if err != nil {
+		return
+	}
+	// 4. 向上游返回数据
+	if _, err := bufio.NewReader(response.Body).WriteTo(responseWriter); err != nil {
+		responseWriter.WriteHeader(http.StatusBadGateway)
+		return
+	}
+
+}
+
+func main() {
+	server := &ProxyServer{proxyServerAddr}
+	server.Run()
+
+}
