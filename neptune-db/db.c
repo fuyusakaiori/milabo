46e417c847addb5ab5be5ececa7c93cdf3250cad
fuyusakaiori <670232228@qq.com>
2024-02-06 17:56:33 +0800

    chapter 4

--- neptune-db/db.c
+++ neptune-db/db.c
@@ -20,8 +20,8 @@ typedef struct {
     // 主键
     uint32_t id;
     // 内容
-    char username[COLUMN_USERNAME_SIZE];
-    char email[COLUMN_EMAIL_SIZE];
+    char username[COLUMN_USERNAME_SIZE + 1];
+    char email[COLUMN_EMAIL_SIZE + 1];
 } Row;
 
 const uint32_t ID_SIZE = size_of_attribute(Row, id);
@@ -166,7 +166,9 @@ typedef enum {
 // 初始化 SQL 结果
 typedef enum {
     PREPARE_SUCCESS,
+    PREPARE_STRING_TOO_LONG,
     PREPARE_SYNTAX_ERROR,
+    PREPARE_NEGATIVE_ID,
     PREPARE_UNRECOGNIZED_STATEMENT
 } PrepareResult;
 
@@ -199,21 +201,43 @@ MetaCommandResult do_meta_command(InputBuffer *input_buffer, Table* table) {
     return META_COMMAND_UNRECOGNIZED_COMMAND;
 }
 
-// 准备 SQL 语句
+// 初始化插入 SQL 语句
+PrepareResult prepare_insert(InputBuffer* input_buffer, Statement* statement) {
+    // 1. 指定 SQL 语句类型
+    statement->type = STATEMENT_INSERT;
+    // 2. 读取缓冲区中的关键字
+    char* keyword = strtok(input_buffer->buffer, " ");
+    char* id_string = strtok(NULL, " ");
+    char* username = strtok(NULL, " ");
+    char* email = strtok(NULL, " ");
+    // 3. 判断字段是否为空
+    if (id_string == NULL || username == NULL || email == NULL) {
+        return PREPARE_SYNTAX_ERROR;
+    }
+    // 4. 转换 id
+    int id = atoi(id_string);
+    if (id < 0) {
+        return PREPARE_NEGATIVE_ID;
+    }
+    // 5. 判断 username 是否超出限制
+    if (strlen(username) > COLUMN_USERNAME_SIZE) {
+        return PREPARE_STRING_TOO_LONG;
+    }
+    if (strlen(email) > COLUMN_EMAIL_SIZE) {
+        return PREPARE_STRING_TOO_LONG;
+    }
+    statement->row_to_insert.id = id;
+    // TODO 为什么不直接赋值呢?
+    strcpy(statement->row_to_insert.username, username);
+    strcpy(statement->row_to_insert.email, email);
+    return PREPARE_SUCCESS;
+}
+
+// 初始化 SQL 语句
 PrepareResult prepare_statement(InputBuffer *input_buffer, Statement *statement) {
     // 1. 判断是否为插入语句
     if (strncmp(input_buffer->buffer, INSERT, 6) == 0) {
-        // 1.1 更新 SQL 语句类型为插入类型
-        statement->type = STATEMENT_INSERT;
-        // 1.2 解析 SQL
-        int args_assigned = sscanf(input_buffer->buffer, "insert %d %s %s",
-                                   &(statement->row_to_insert.id), &(statement->row_to_insert.username), &(statement->row_to_insert.email));
-        // 1.3 判断是否语法错误
-        if (args_assigned < 3) {
-            return PREPARE_SYNTAX_ERROR;
-        }
-        // 1.2 返回准备完成的结果
-        return PREPARE_SUCCESS;
+        return prepare_insert(input_buffer, statement);
     }
     // 2. 判断是否为查询语句
     if (strncmp(input_buffer->buffer, SELECT, 6) == 0) {
@@ -224,7 +248,6 @@ PrepareResult prepare_statement(InputBuffer *input_buffer, Statement *statement)
     }
     return PREPARE_UNRECOGNIZED_STATEMENT;
 }
-
 
 // 执行 SQL 插入语句
 ExecuteResult execute_insert(Statement* statement, Table* table) {
@@ -289,6 +312,12 @@ int main() {
         switch (prepare_statement(input_buffer, &statement)) {
             case PREPARE_SUCCESS:
                 break;
+            case PREPARE_NEGATIVE_ID:
+                printf("id must be positive. \n");
+                continue;
+            case PREPARE_STRING_TOO_LONG:
+                printf("string is too long. \n");
+                continue;
             case PREPARE_SYNTAX_ERROR:
                 printf("syntax error. could not parse statement. \n");
                 continue;
