d2607a41aa91e22b0d9c55b96e4f94a286999ff5
fuyusakaiori <670232228@qq.com>
2024-03-14 22:41:02 +0800

    chapter 10

--- neptune-db/db.c
+++ neptune-db/db.c
@@ -168,6 +168,7 @@ const uint32_t PARENT_POINTER_OFFSET = IS_ROOT_OFFSET + IS_ROOT_SIZE;
 
 // 树节点通用头信息的大小：为什么原本用的是 uint8_t 类型？为什么计算 uint8_t 的大小还要使用 uint32_t 接收？
 const uint32_t COMMON_TREE_NODE_HEADER_SIZE = TREE_NODE_TYPE_SIZE + IS_ROOT_SIZE + PARENT_POINTER_SIZE;
+
 /*
  * 树叶子节点头信息的内存布局
  */
@@ -178,6 +179,7 @@ const uint32_t TREE_LEAF_NODE_NUM_CELLS_OFFSET = COMMON_TREE_NODE_HEADER_SIZE;
 
 // 树的叶子节点的头信息大小
 const uint32_t COMMON_TREE_LEAF_NODE_HEADER_SIZE = COMMON_TREE_NODE_HEADER_SIZE + TREE_LEAF_NODE_NUM_CELLS_SIZE;
+
 /*
  * 树叶子节点存储的内容的内存布局
  */
@@ -197,6 +199,41 @@ const uint32_t TREE_LEAF_NODE_SPACE_FOR_CELLS = PAGE_SIZE - COMMON_TREE_LEAF_NOD
 // 树的叶子节点最多存储的关键字数量
 const uint32_t TREE_LEAF_NODE_MAX_CELLS = TREE_LEAF_NODE_SPACE_FOR_CELLS / TREE_LEAF_NODE_CELL_SIZE;
 
+// 分裂节点时新节点的关键字数量
+const uint32_t TREE_LEAF_NODE_RIGHT_SPLIT_COUNT = (TREE_LEAF_NODE_MAX_CELLS + 1) / 2;
+// 分裂节点时旧节点的关键字数量
+const uint32_t TREE_LEAF_NODE_LEFT_SPLIT_COUNT = (TREE_LEAF_NODE_MAX_CELLS + 1) - TREE_LEAF_NODE_RIGHT_SPLIT_COUNT;
+
+/*
+ * 树非叶子节点的头信息内存布局
+ * TODO 为什么没有左子节点?
+ */
+// 内部节点的关键字数量的字段大小
+const uint32_t TREE_INTERNAL_NODE_NUM_KEYS_SIZE = sizeof(uint32_t);
+// 内部节点的关键字数量的字段的偏移量
+const uint32_t TREE_INTERNAL_NODE_NUM_KEYS_OFFSET = COMMON_TREE_NODE_HEADER_SIZE;
+// 内部节点的右子节点的页号字段大小
+const uint32_t TREE_INTERNAL_NODE_RIGHT_CHILD_SIZE = sizeof(uint32_t);
+// 内部节点的右子节点的页号字段的偏移量
+const uint32_t TREE_INTERNAL_NODE_RIGHT_CHILD_OFFSET =
+        TREE_INTERNAL_NODE_NUM_KEYS_OFFSET + TREE_INTERNAL_NODE_NUM_KEYS_SIZE;
+// 内部节点的头信息大小
+const uint32_t INTERNAL_NODE_HEADER_SIZE = COMMON_TREE_NODE_HEADER_SIZE +
+        TREE_INTERNAL_NODE_NUM_KEYS_SIZE +
+        TREE_INTERNAL_NODE_RIGHT_CHILD_SIZE;
+
+/*
+ * 树非叶子节点的内存布局
+ */
+// 内部节点的关键字 key 字段的大小
+const uint32_t TREE_INTERNAL_NODE_KEY_SIZE = sizeof(uint32_t);
+// 内部节点的关键字 value 字段的大小, 也就是子节点的指针
+const uint32_t TREE_INTERNAL_NODE_CHILD_SIZE = sizeof(uint32_t);
+// 内部节点关键字的大小
+const uint32_t TREE_INTERNAL_NODE_CELL_SIZE =
+        TREE_INTERNAL_NODE_KEY_SIZE + TREE_INTERNAL_NODE_CHILD_SIZE;
+
+
 // 初始化缓冲区
 InputBuffer *new_input_buffer();
 // 命令行提示输入
@@ -215,20 +252,46 @@ void print_row(Row* row);
 uint32_t* leaf_node_num_cells(void* node);
 // 获取叶子节点中的关键字的 key
 uint32_t* leaf_node_key(void* node, uint32_t cell_num);
-// 初始化叶子节点的数量
+// 初始化叶子节点
 void initialize_leaf_node(void* node);
+// 获取内部节点的关键字数量
+uint32_t* internal_node_num_keys(void* node);
+// 获取内部节点最后一个关键字
+// 每个内部节点的关键字数量如果是 m, 那么每个内部节点就会有 m + 1 个子节点
+// 这也就意味着最后一个关键字只能单独存放到一边
+uint32_t* internal_node_right_child(void* node);
+// 获取内部节点的关键字
+uint32_t* internal_node_cell(void* node, uint32_t cell_num);
+// 获取内部节点的关键字的 key
+uint32_t* internal_node_key(void* node, uint32_t key_num);
+// 获取内部节点的关键字的 value - 子节点的指针
+uint32_t* internal_node_child(void* node, uint32_t child_num);
+// 初始化内部节点
+void initialize_internal_node(void* node);
+// 创建新的节点, 也就是父节点
+void create_new_root(Table* table, uint32_t right_child_page_num);
 // 插入叶子节点
 void leaf_node_insert(Cursor* cursor, uint32_t key, Row* value);
+// 分裂并插入叶子节点
+void leaf_node_split_and_insert(Cursor* cursor, uint32_t key, Row* value);
 // 查询叶子节点
 Cursor* leaf_node_find(Table* table, uint32_t page_num, uint32_t key);
 // 获取节点类型
 TreeNodeType get_node_type(void* node);
 // 设置节点类型
 void set_node_type(void* node, TreeNodeType type);
+// 获取节点中最大的关键字
+uint32_t get_node_max_key(void* node);
+// 设置根节点
+void set_node_root(void* node, bool is_root);
+// 判断是否为根节点
+bool is_node_root(void* node);
 // 初始化内存调度器
 Pager* pager_open(const char* filename);
 // 调度磁盘数据到页内存中
 void* get_page(Pager* pager, uint32_t page_num);
+// 查询未使用的页内存的页号
+uint32_t get_unused_page_num(Pager* pager);
 // 持久化页内存的数据到磁盘中
 void pager_flush(Pager* pager, uint32_t page_num);
 // 初始化表
@@ -308,7 +371,6 @@ void print_row(Row* row) {
     printf("(%d, %s, %s)\n", row->id, row->username, row->email);
 }
 
-
 // 获取叶子节点中的关键字数量
 uint32_t* leaf_node_num_cells(void* node) {
     // 基址 + 关键字数量字段的偏移量
@@ -336,8 +398,89 @@ void* leaf_node_value(void* node, uint32_t cell_num) {
 void initialize_leaf_node(void* node) {
     // 初始化节点关键字数量
     *leaf_node_num_cells(node) = 0;
+    // 设置是否为根节点
+    set_node_root(node, false);
     // 设置节点类型
     set_node_type(node, TREE_NODE_LEAF);
+}
+
+// 获取内部节点的关键字数量
+uint32_t* internal_node_num_keys(void* node) {
+    // TODO 为什么 void* 可以直接隐式转换成 uint32_t*
+    return node + TREE_INTERNAL_NODE_NUM_KEYS_OFFSET;
+}
+
+// 获取内部节点最后一个关键字
+uint32_t* internal_node_right_child(void* node) {
+    return node + TREE_INTERNAL_NODE_RIGHT_CHILD_OFFSET;
+}
+
+// 获取内部节点的关键字
+uint32_t* internal_node_cell(void* node, uint32_t cell_num) {
+    return node + INTERNAL_NODE_HEADER_SIZE + cell_num * TREE_INTERNAL_NODE_CELL_SIZE;
+}
+
+// 获取内部节点的关键字的 key
+uint32_t* internal_node_key(void* node, uint32_t key_num) {
+    // 注: 优先存放的是指针而不是 key, 为什么要这么存呢?
+    return internal_node_cell(node, key_num) + TREE_INTERNAL_NODE_CHILD_SIZE;
+}
+
+// 获取内部节点的关键字的 value - 子节点的指针
+uint32_t* internal_node_child(void* node, uint32_t child_num) {
+    // 1. 计算现在内部节点有多少关键字
+    uint32_t num_keys = *internal_node_num_keys(node);
+    // 2. 判断是否访问是否超过界限
+    if (child_num > num_keys) {
+        printf("tried to access child_num %d > num_keys %d\n", child_num, num_keys);
+        exit(EXIT_FAILURE);
+    } else if (child_num == num_keys) {
+        // 3. 获取最后一个关键字
+        return internal_node_right_child(node);
+    } else {
+        // 4. 如果不是最后一个关键字, 那么直接访问就行
+        return internal_node_cell(node, child_num);
+    }
+}
+
+// 初始化内部节点
+void initialize_internal_node(void* node) {
+    // 1. 设置是否为根节点
+    set_node_root(node, false);
+    // 2. 设置节点类型
+    set_node_type(node, TREE_NODE_INTERNAL);
+    // 3. 设置节点的关键字数量
+    *internal_node_num_keys(node) = 0;
+}
+
+// 创建新的节点
+void create_new_root(Table* table, uint32_t right_child_page_num) {
+    // 1. 获取原本的根节点
+    void* root = get_page(table->pager, table->root_page_num);
+    // 2. 获取新的节点, 也就是右边的节点, 好像没有用到?
+    void* right_child = get_page(table->pager, right_child_page_num);
+    // 3. 为左边的节点找一块新的内存
+    uint32_t left_child_page_num = get_unused_page_num(table->pager);
+    // 4. 获取新的节点, 但是是左边的节点
+    void* left_child = get_page(table->pager, left_child_page_num);
+    // 5. 将原本根节点的内容拷贝到左边的节点
+    memcpy(left_child, root, PAGE_SIZE);
+    // 6. 设置左边的节点为非根节点
+    set_node_root(left_child, false);
+    // 7. 重新初始化根节点
+    initialize_internal_node(root);
+    // 8. 重新设置旧的节点为根节点: 原来初始化的时候是叶子节点
+    set_node_root(root, true);
+    // 9. 重新设置根节点关键字数量为 1, 因为就分裂了两个节点, 所以根节点只有一个关键字
+    *internal_node_num_keys(root) = 1;
+    // 10. 设置根节点的关键字的 value - 子节点的指针, 或者说页号
+    *internal_node_child(root, 0) = left_child_page_num;
+    // 11. 获取左子节点的最大关键字
+    uint32_t left_child_max_key = get_node_max_key(left_child);
+    // 12. 重新设置根节点的关键字的 key
+    *internal_node_key(root, 0) = left_child_max_key;
+    // 13. 最后再将最后一个子节点设置到根节点中
+    *internal_node_right_child(root) = right_child_page_num;
 }
 
 // 插入叶子节点
@@ -348,8 +491,9 @@ void leaf_node_insert(Cursor* cursor, uint32_t key, Row* value) {
     uint32_t cell_nums = *leaf_node_num_cells(node);
     // 3. 判断节点的关键字数量是否超过限制, 需要分裂
     if (cell_nums >= TREE_LEAF_NODE_MAX_CELLS) {
-        printf("need to implement splitting a leaf node. \n");
-        exit(EXIT_FAILURE);
+        // 注: 叶子节点分裂
+        leaf_node_split_and_insert(cursor, key, value);
+        return;
     }
     // 4. 判断应该在哪里插入关键字
     if (cursor->cell_num < cell_nums) {
@@ -365,6 +509,47 @@ void leaf_node_insert(Cursor* cursor, uint32_t key, Row* value) {
     *(leaf_node_key(node, cursor->cell_num)) = key;
     // 7. 保存关键字的 value: void* 类型无法赋值, 只能拷贝
     serialize_row(value, leaf_node_value(node, cursor->cell_num));
+}
+
+// 分裂并插入叶子节点
+void leaf_node_split_and_insert(Cursor* cursor, uint32_t key, Row* value) {
+    // 1. 获取需要分裂的节点, 也就是当前节点
+    void* old_node = get_page(cursor->table->pager, cursor->page_num);
+    // 2. 找到还没有使用的页内存, 作为新的节点
+    uint32_t new_page_num = get_unused_page_num(cursor->table->pager);
+    // 3. 获取分裂后的新节点
+    void* new_node = get_page(cursor->table->pager, new_page_num);
+    // 4. 初始化新生成的节点
+    initialize_leaf_node(new_node);
+    // 5. 将旧节点中一半的关键字都移动到新节点中
+    for (uint32_t index = TREE_LEAF_NODE_MAX_CELLS; index >= 0; index--) {
+        // 5.1 根据分裂的阈值判断目的节点是谁
+        void* destination_node = index > TREE_LEAF_NODE_LEFT_SPLIT_COUNT ? new_node: old_node;
+        // 5.2 重新定位关键字在目的节点中的位置
+        uint32_t index_within_node = index % TREE_LEAF_NODE_LEFT_SPLIT_COUNT;
+        // 5.3 获取关键字应该在的地址
+        void* destination = leaf_node_cell(destination_node, index_within_node);
+        // 5.4 如果是新插入的关键字, 那么需要序列化到节点中
+        if (index == cursor->cell_num) {
+            serialize_row(value, destination);
+        } else if (index > cursor->cell_num) {
+            // 5.5 如果需要移动的节点比新插入的关键字大, 那么就将 index - 1 的位置拷贝过去, 因为索引是从长度开始算的
+            memcpy(destination, leaf_node_cell(old_node, index - 1), TREE_LEAF_NODE_CELL_SIZE);
+        } else {
+            // 5.6 如果需要移动的节点比新插入的关键字小, 那么就将 index 的位置拷贝过去, 因为已经新插入了一个关键字, 所以直接使用索引是对的
+            memcpy(destination, leaf_node_cell(old_node, index), TREE_LEAF_NODE_CELL_SIZE);
+        }
+    }
+    // 6. 更新每个节点的关键字数量
+    *(leaf_node_num_cells(old_node)) = TREE_LEAF_NODE_LEFT_SPLIT_COUNT;
+    *(leaf_node_num_cells(new_node)) = TREE_LEAF_NODE_RIGHT_SPLIT_COUNT;
+    // 7. 生成父节点
+    if (is_node_root(old_node)) {
+        return create_new_root(cursor->table, new_page_num);
+    } else {
+        printf("need to implement updating parent after split\n");
+        exit(EXIT_FAILURE);
+    }
 }
 
 // 查询叶子节点
@@ -412,6 +597,32 @@ TreeNodeType get_node_type(void* node) {
 void set_node_type(void* node, TreeNodeType type) {
     uint8_t value = type;
     *((uint8_t*) node + TREE_NODE_TYPE_OFFSET) = value;
+}
+
+// 获取节点最大的关键字
+uint32_t get_node_max_key(void* node) {
+    switch (get_node_type(node)) {
+        case TREE_NODE_INTERNAL:
+            // 内部节点的最大关键字其实也是最后一个
+            return *internal_node_key(node, *internal_node_num_keys(node) - 1);
+        case TREE_NODE_LEAF:
+            // 叶子节点最大的关键字一定是最后一个
+            return *leaf_node_key(node, *leaf_node_num_cells(node) - 1);
+    }
+}
+
+// 设置根节点
+void set_node_root(void* node, bool is_root) {
+    *((uint8_t *) (node + IS_ROOT_OFFSET)) = is_root;
+}
+
+// 判断是否为根节点
+bool is_node_root(void* node) {
+    // 1. 取出保存的是否为根节点的布尔值
+    // 注: 保存的时候是 4B, 是作者觉得比较方便, 读出来的时候就转换成 1B, 因为布尔值只需要 1B
+    uint8_t value = *((uint8_t*)(node + IS_ROOT_OFFSET));
+    // 2. 返回布尔值
+    return (bool) value;
 }
 
 // 初始化内存调度器
@@ -485,6 +696,12 @@ void* get_page(Pager* pager, uint32_t page_num) {
     return pager->pages[page_num];
 }
 
+// 获取未使用的页内存的页号
+uint32_t get_unused_page_num(Pager* pager) {
+    // 注: 暂时直接使用最后一页就行
+    return pager->num_pages;
+}
+
 // 持久化页内存的数据到磁盘中
 void pager_flush(Pager* pager, uint32_t page_num) {
     // 1. 判断页内存指针是否为空
@@ -518,8 +735,11 @@ Table* db_open(const char* filename) {
     table->root_page_num = 0;
     // 4. 判断页数量是否为空
     if (pager->num_pages == 0) {
+        void* root_node = get_page(pager, 0);
         // 初始化第 0 页的内存
-        initialize_leaf_node(get_page(pager, 0));
+        initialize_leaf_node(root_node);
+        // 因为这是第一个节点, 虽然是叶子节点, 但是同时也是根节点
+        set_node_root(root_node, true);
     }
     // 5. 赋值内存调度器
     table->pager = pager;
@@ -704,26 +924,22 @@ ExecuteResult execute_insert(Statement* statement, Table* table) {
     void* node = get_page(table->pager, table->root_page_num);
     // 2. 获取关键字数量
     uint32_t num_cells = (*(leaf_node_num_cells(node)));
-    // 3. 判断关键字数量是否超过限制
-    if (num_cells > TREE_LEAF_NODE_MAX_CELLS) {
-        return EXECUTE_TABLE_FULL;
-    }
-    // 4. 获取需要插入的行记录
+    // 3. 获取需要插入的行记录
     Row* row_to_insert = &(statement->row_to_insert);
-    // 5. 获取 id
+    // 4. 获取 id
     uint32_t key_to_insert = row_to_insert->id;
-    // 6. 游标移动需要插入的位置
+    // 5. 游标移动需要插入的位置
     Cursor *cursor = table_find(table, key_to_insert);
-    // 7. 判断游标的位置是否是在末尾
+    // 6. 判断游标的位置是否是在末尾
     if (cursor->cell_num < num_cells) {
-        // 8. 获取关键字的 key
+        // 7. 获取关键字的 key
         uint32_t key_at_index = *leaf_node_key(node, cursor->cell_num);
-        // 9. 如果游标的位置不在末尾, 那么就需要判断 id 是否重复
+        // 8. 如果游标的位置不在末尾, 那么就需要判断 id 是否重复
         if (key_at_index == key_to_insert) {
             return EXECUTE_DUPLICATE_KEY;
         }
     }
-    // 10. 如果游标就在末尾, 那么直接插入就行, 肯定是新的
+    // 9. 如果游标就在末尾, 那么直接插入就行, 肯定是新的
     leaf_node_insert(cursor, row_to_insert->id, row_to_insert);
     return EXECUTE_SUCCESS;
 }
